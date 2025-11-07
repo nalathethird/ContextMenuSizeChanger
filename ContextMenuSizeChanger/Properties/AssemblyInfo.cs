ca234ef9cc27bb5ee31f4b7c7b60b246485c9780
Zeia Nala <36301692+nalathethird@users.noreply.github.com>
2025-11-07 12:08:13 -0600

    Delete ContextMenuSizeChanger directory

--- ContextMenuSizeChanger/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("ContextMenuSizeChanger")]
-[assembly: AssemblyProduct("ContextMenuSizeChanger")]
-[assembly: AssemblyDescription("A Resonite Mod to change the default size of the context menu")]
-[assembly: AssemblyCompany("NalaTheThird")]
-[assembly: AssemblyCopyright("Copyright © 2025 NalaTheThird")]
-[assembly: AssemblyVersion(ContextMenuSizeChanger.ContextMenuSizeChanger.VERSION_CONSTANT)]
-[assembly: AssemblyFileVersion(ContextMenuSizeChanger.ContextMenuSizeChanger.VERSION_CONSTANT)]
